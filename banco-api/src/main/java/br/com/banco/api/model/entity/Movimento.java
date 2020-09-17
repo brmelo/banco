3523992ead7bb4f4cb74603f18f06e3bd82cafb5
Allysson Melo <allysson_brunno@yahoo.com.br>
2020-09-17 14:53:14 -0300

    Criação do projeto, repository, enum

---
+++ banco-api/src/main/java/br/com/banco/api/model/entity/Movimento.java
@@ -0,0 +1,96 @@
+package br.com.banco.api.model.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import br.com.banco.api.model.entity.enums.TipoMovimento;
+
+@Entity
+public class Movimento {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private TipoMovimento tipoMovimento;
+	
+	private BigDecimal valor;
+
+	@ManyToOne
+	@JoinColumn(name = "id_conta")
+	private Conta conta;
+
+	public Movimento() {
+	}
+
+	public Movimento(Integer id, TipoMovimento tipoMovimento, BigDecimal valor, Conta conta) {
+		this.id = id;
+		this.tipoMovimento = tipoMovimento;
+		this.valor = valor;
+		this.conta = conta;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public TipoMovimento getTipoMovimento() {
+		return tipoMovimento;
+	}
+
+	public void setTipoMovimento(TipoMovimento tipoMovimento) {
+		this.tipoMovimento = tipoMovimento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movimento other = (Movimento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
---
+++ banco-api/src/main/java/br/com/banco/api/model/entity/enums/TipoMovimento.java
@@ -0,0 +1,7 @@
+package br.com.banco.api.model.entity.enums;
+
+public enum TipoMovimento {
+
+	DEPOSITO, SAQUE, TRANSFERENCIA;
+	
+}
